50a6027bd86b2e0c6f7cfe5eeb70052e271edf40
Soe Min Htike <student.ucsm@gmail.com>
2015-07-01 09:01:36 +0630

    initial commit

---
+++ String.h
@@ -0,0 +1,26 @@
+class String{
+private : 
+	const char* ch;
+	int length;
+	
+public :
+	String();
+	String(const char* ch);
+	
+	String operator+(String);
+	String operator+(int);
+	String operator+(float);
+	String operator+(double);
+	String operator+(long);
+	friend String operator+(int , String);
+	friend String operator+(float, String);
+	friend String operator+(double, String);
+	friend String operator+(long, String);
+	String operator+(char);
+	friend String operator+(char , String);
+	String operator+(const char*);
+	friend String operator+(const char* , String);
+	
+	String subString(int startIndex, int endIndex);
+	char charAt(int index);
+};
